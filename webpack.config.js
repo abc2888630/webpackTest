f909e0f3f25d8cbde19b05d1823ee24e6c2ab50f
51fan <abc2978723@163.com>
2018-01-26 09:34:51 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 let htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry:__dirname +'/src/app.js',
+	entry:__dirname +'/src/app/app.js',
 	output:{
 		path: __dirname + "/dist",//打包后的文件存放的地方
     	filename: "js/[name].bundle.js"//打包后输出文件的文件名
 	},
-	plguins:[
+	plugins:[
 		new htmlWebpackPlugin({
 			filename:'index.html',
 			template:'index.html',
@@ -19,7 +19,7 @@ module.exports = {
 				test:/\.js$/,
 				loader:'babel',
 				query:{
-					presets:['latset']
+					presets:['latest']
 				}
 			}
 		]
